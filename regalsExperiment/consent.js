5b184a3ff2000e274b3f5716ad6870645d57789e
Eike Schneiders <eikeschneiders@hotmail.com>
2024-05-28 12:10:46 +0100

    Restructuring files

---
+++ regalsExperiment/consent.js
@@ -0,0 +1,12 @@
+var consentQuestion = {
+    type: jsPsychInstructions,
+    pages: ['By clicking \"I agree\", you affirm that you are at least 18 years of age, which is the minimum age to participate in this study, and that you understand the nature of your participation in this research.<p>If you do not wish to participate, please close this window.'],
+    show_clickable_nav: true,
+    key_forward: " ",
+    button_label_next: "I agree",
+    allow_backward: true,
+    data: {
+        Condition: 'REMOVE', // For easier data processing
+        Running: 'Consent'
+      }
+  }
