1b5497f11261c8255cb13f2f70b6ff3c0f59c782
Eike Schneiders <eikeschneiders@hotmail.com>
2024-05-28 13:40:12 +0100

    Lawyer cases

---
+++ regalsExperiment/lawyerCases.js
@@ -0,0 +1,87 @@
+var tax_cases_Lawyer = [
+    `<p>Tax Lawyer 1</p>
+    <br>
+  
+    <h3>Advice</h3>
+    <p>Advice Tax Lawyer 1</p>
+
+    <h3 style="text-align: center">In the described situation, I would feel 
+      confident acting on the advice provided:</h3><br>`,
+
+    `<p>Tax Lawyer 2</p>
+    <br>
+  
+    <h3>Advice</h3>
+    <p>Advice Tax Lawyer 2</p>
+
+    <h3 style="text-align: center">In the described situation, I would feel 
+      confident acting on the advice provided:</h3><br>`,
+
+    `<p>Tax Lawyer 3</p>
+      <br>
+    
+      <h3>Advice</h3>
+      <p>Advice Tax Lawyer 3</p>
+  
+    <h3 style="text-align: center">In the described situation, I would feel 
+        confident acting on the advice provided:</h3><br>`,
+]
+
+var plan_cases_Lawyer = [
+    `<p>Plan Lawyer 1</p>
+    <br>
+  
+    <h3>Advice</h3>
+    <p>Advice Plan Lawyer 1</p>
+
+    <h3 style="text-align: center">In the described situation, I would feel 
+      confident acting on the advice provided:</h3><br>`,
+
+    `<p>Plan Lawyer 2</p>
+    <br>
+  
+    <h3>Advice</h3>
+    <p>Advice Plan Lawyer 2</p>
+
+    <h3 style="text-align: center">In the described situation, I would feel 
+      confident acting on the advice provided:</h3><br>`,
+
+    `<p>Plan Lawyer 3</p>
+      <br>
+    
+      <h3>Advice</h3>
+      <p>Advice Plan Lawyer 3</p>
+  
+    <h3 style="text-align: center">In the described situation, I would feel 
+        confident acting on the advice provided:</h3><br>`,
+]
+
+var prop_cases_Lawyer = [
+    `<p>Property Lawyer 1</p>
+    <br>
+  
+    <h3>Advice</h3>
+    <p>Advice Property Lawyer 1</p>
+
+    <h3 style="text-align: center">In the described situation, I would feel 
+      confident acting on the advice provided:</h3><br>`,
+
+    `<p>Property Lawyer 2</p>
+    <br>
+  
+    <h3>Advice</h3>
+    <p>Advice Property Lawyer 2</p>
+
+    <h3 style="text-align: center">In the described situation, I would feel 
+      confident acting on the advice provided:</h3><br>`,
+
+    `<p>Property Lawyer 3</p>
+      <br>
+    
+      <h3>Advice</h3>
+      <p>Advice Property Lawyer 3</p>
+  
+    <h3 style="text-align: center">In the described situation, I would feel 
+        confident acting on the advice provided:</h3><br>`,
+]
+
